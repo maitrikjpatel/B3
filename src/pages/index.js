97e9acc89370de73af50fa3496c1c62943bd9884
Maitrik Patel <maitrik1419@gmail.com>
2019-03-17 16:02:41 -0700

    update images

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,21 @@ import Layout from '../components/layout'
 // import Lightbox from 'react-images'
 import Gallery from '../components/Gallery'
 
-import thumb01 from '../assets/images/thumbs/01.jpg'
-import thumb02 from '../assets/images/thumbs/02.jpg'
-import thumb03 from '../assets/images/thumbs/03.jpg'
-import thumb04 from '../assets/images/thumbs/04.jpg'
-import thumb05 from '../assets/images/thumbs/05.jpg'
-import thumb06 from '../assets/images/thumbs/06.jpg'
+import thumb01 from '../assets/images/thumbs/1.jpg'
+import thumb02 from '../assets/images/thumbs/2.jpg'
+import thumb03 from '../assets/images/thumbs/3.jpg'
+import thumb04 from '../assets/images/thumbs/4.jpg'
+import thumb05 from '../assets/images/thumbs/5.jpg'
+import thumb06 from '../assets/images/thumbs/6.jpg'
 
-import full01 from '../assets/images/fulls/01.jpg'
-import full02 from '../assets/images/fulls/02.jpg'
-import full03 from '../assets/images/fulls/03.jpg'
-import full04 from '../assets/images/fulls/04.jpg'
-import full05 from '../assets/images/fulls/05.jpg'
-import full06 from '../assets/images/fulls/06.jpg'
+import full01 from '../assets/images/fulls/1.jpg'
+import full02 from '../assets/images/fulls/2.jpg'
+import full03 from '../assets/images/fulls/3.jpg'
+import full04 from '../assets/images/fulls/4.jpg'
+import full05 from '../assets/images/fulls/5.jpg'
+import full06 from '../assets/images/fulls/6.jpg'
+import full07 from '../assets/images/fulls/7.jpg'
+import full08 from '../assets/images/fulls/8.jpg'
 
 const DEFAULT_IMAGES = [
     { id: '1', src: full01, thumbnail: thumb01, caption: 'Photo 1', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
@@ -25,7 +27,9 @@ const DEFAULT_IMAGES = [
     { id: '3', src: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
     { id: '4', src: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
     { id: '5', src: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '6', src: full06, thumbnail: thumb06, caption: 'Photo 6', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'}
+    { id: '6', src: full06, thumbnail: thumb06, caption: 'Photo 6', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '7', src: full07, thumbnail: thumb06, caption: 'Photo 7', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '8', src: full08, thumbnail: thumb06, caption: 'Photo 8', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'}
 ];
 
 class HomeIndex extends React.Component {
@@ -87,7 +91,7 @@ class HomeIndex extends React.Component {
 
                 <div id="main">
 
-                    <section id="one">
+                    {/* <section id="one">
                         <header className="major">
                             <h2>Ipsum lorem dolor aliquam ante commodo<br />
                             magna sed accumsan arcu neque.</h2>
@@ -96,7 +100,7 @@ class HomeIndex extends React.Component {
                         <ul className="actions">
                             <li><a href="#" className="button">Learn More</a></li>
                         </ul>
-                    </section>
+                    </section> */}
 
                     <section id="two">
                         <h2>Recent Work</h2>
