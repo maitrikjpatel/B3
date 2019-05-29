773690ecaafe15befb8cf7c59d88567868d1ef46
Maitrik Patel <maitrik.patel@docusign.com>
2019-05-28 23:12:27 -0700

    update header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../assets/scss/main.scss'
 
-import Header from './header'
+import Header from './Header'
 
 class Template extends React.Component {
     render() {
