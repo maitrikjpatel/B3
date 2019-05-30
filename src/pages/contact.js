d36737e39cbbb485b5bade30e486172a07699635
Maitrik Patel <maitrik.patel@docusign.com>
2019-05-30 02:04:37 -0700

    update form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -49,8 +49,11 @@ class ContactPate extends React.Component {
                             name="contact"
                             method="post" 
                             action="#"
-                            data-netlify="true"
                             onSubmit={this.handleSubmit} 
+                            netlify
+                            data-netlify="true"
+                            netlify-honeypot="bot-field"
+                            data-netlify-recaptcha="true"
                             data-netlify-honeypot="bot-field">
                                 <p>Contact To Start Your Custom Order</p>
                                 <div className="row uniform 50%">
