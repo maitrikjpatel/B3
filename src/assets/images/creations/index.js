ab509a664a0eb2547578c98bcb1c4c628ab9decb
Maitrik Patel <maitrik.patel@docusign.com>
2019-06-02 10:39:44 -0700

    update image count

--- src/assets/images/creations/index.js
+++ src/assets/images/creations/index.js
@@ -5,21 +5,27 @@ let AllFullImages = importAll(require.context('./fulls', true, /\.(png|jpe?g|svg
 let AllThumbImages = importAll(require.context('./thumbs', true, /\.(png|jpe?g|svg)$/));
 
 let images = [];
-let totalImagesNumber = 58;
+let totalImagesNumber = 1000;
 
 for(let i = 1; i < totalImagesNumber; i++) {
 
     let imgObj;
     
-    imgObj = {
-        id: i, 
-        src: AllFullImages[`Full${[i]}.jpg`], 
-        thumbnail: AllThumbImages[`Thumb${[i]}.jpg`],  
-        caption: `Cake${[i]}`,  
-        description: `Cake${[i]}`, 
+    if(!AllFullImages[`Full${[i]}.jpg`]) {
+        break;
+    }
+    else {
+        imgObj = {
+            id: i, 
+            src: AllFullImages[`Full${[i]}.jpg`], 
+            thumbnail: AllThumbImages[`Thumb${[i]}.jpg`],  
+            caption: `Cake${[i]}`,  
+            description: `Cake${[i]}`, 
+        }
+        
+        images.push(imgObj)
     }
     
-    images.push(imgObj)
 }
 
 export default images
